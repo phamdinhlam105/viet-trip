d2d63be34922eaa5d528b8c11cb96820b9ab63f6
Phạm Đình Lâm <62865687+phamdinhlam105@users.noreply.github.com>
2025-06-28 18:55:34 +0700

    Update logo.tsx

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return <Image src="/homepage/MainLogo.svg" alt="Logo chính" fill/>
+    return <Image src="/homepage/MainLogo.svg" alt="Logo chính" width={300} height={200} className="object-cover"/>
 }
