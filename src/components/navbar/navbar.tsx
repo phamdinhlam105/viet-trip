1805004e1c7d9e3b38721a56220e7ceb28fb2da1
Phạm Đình Lâm <62865687+phamdinhlam105@users.noreply.github.com>
2025-06-28 18:49:40 +0700

    Update navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,26 +12,26 @@ import { useState } from "react";
 export default function NavigationBar() {
 
     const [isOpen, setIsOpen] = useState(false);
-    return <nav className="w-full h-28 border-radius-md shadow-md fixed top-0 z-10 bg-white">
+    return <nav className="w-full h-28 shadow-md fixed top-0 z-10 bg-white">
         <div className="py-2 px-10 bg-black text-white items-center flex justify-between">
             <div className="flex space-x-4 items-center">
                 <Phone /> <span>084 246 6868</span>
             </div>
-            <div className="flex space-x-4 items-centertext-md">
+            <div className="flex space-x-4 items-center text-md">
                 <MapPin /> <span>135/8 Nguyễn Thái Học, Nha Trang, Khánh Hoà</span>
             </div>
             <div>
                 <Search />
             </div>
         </div>
-        <div className="px-[10%] w-full items-center flex justify-between space-x-2 py-2">
+        <div className="px-[10%] w-full items-center flex justify-between py-2">
             <Link href="/" className="relative w-1/10 aspect-[2/1]">
                 <Logo />
             </Link>
             <div className="w-2/3">
-                <ul className="flex justify-between justify-end space-x-10">
+                <ul className="flex justify-between items-center justify-end space-x-10">
                     {NavBarStructure.map(item =>
-                        <li key={item.id} className="flex justify-between">
+                        <li key={item.id} className="flex justify-between items-center">
                             <a href={item.link} className="text-sm font-semibold text-black uppercase">{item.name}</a>
                         </li>
                     )}
