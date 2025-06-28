5c1fbda939ff1396f0e379993ab8932f2a596586
Phạm Đình Lâm <heartsstealer105@gmail.com>
2025-06-28 10:21:10 +0700

    fix any type

--- src/components/tour-booking/booking-item-card/booking-index.tsx
+++ src/components/tour-booking/booking-item-card/booking-index.tsx
@@ -4,10 +4,9 @@ import HotelCard from "./hotel-card";
 import ComboCard from "./combo-card";
 
 export function BookingItemCard({ item, onRemove }: { item: { id: number, type: string }; onRemove: (index: number) => void }) {
-    const itemData = item as any;
 
-    if (itemData.type === "tour") return <TourCard id={itemData.id} onRemove={onRemove} />;
-    if (itemData.type === "hotel") return <HotelCard id={itemData.id} onRemove={onRemove} />;
-    if (itemData.type === "combo") return <ComboCard id={itemData.id} onRemove={onRemove} />;
+    if (item.type === "tour") return <TourCard id={item.id} onRemove={onRemove} />;
+    if (item.type === "hotel") return <HotelCard id={item.id} onRemove={onRemove} />;
+    if (item.type === "combo") return <ComboCard id={item.id} onRemove={onRemove} />;
     return null;
 }
