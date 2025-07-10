2ced4874c9c2f7936e3248f61832fa5197a41334
Phạm Đình Lâm <62865687+phamdinhlam105@users.noreply.github.com>
2025-07-10 20:41:53 +0700

    update meta data description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Viettrip Tourist",
-  description: "Trung tâm du lịch Viet Trip Tourist",
+  description: "Lữ Hành Quốc Tế Việt Trip",
 
 };
 
