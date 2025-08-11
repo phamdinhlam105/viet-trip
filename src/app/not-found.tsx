df50b938817c2cb7e62ccf3a8fb5ddc0c346b7a0
Phạm Đình Lâm <phamdinhlam105@gmail.com>
2025-08-11 18:00:12 +0700

    update 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,5 @@
 import NotFoundBody from "@/components/not-found/not-found-body";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "404 - Trang không tồn tại | Viettrip Tourist",
-  description:
-    "Trang bạn tìm không tồn tại. Có thể đường dẫn đã bị thay đổi hoặc bị xóa.",
-};
 export default function NotFound() {
+  
   return <NotFoundBody />;
 }
